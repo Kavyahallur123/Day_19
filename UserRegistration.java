99d9ca007092e855c1e9ac8c9a54c9f1ec12cd67
kavyahallur123 <kavyahallur199@gmail.com>
2023-03-02 12:03:00 +0530

    Added: Validate first name

---
+++ UserRegistration.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    public static final Scanner sc = new Scanner(System.in);
+
+    public static void validFirstName() {
+        System.out.println("Enter the First Name: ");
+        String name = sc.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        boolean result = matcher.matches();
+
+        if (result) {
+            System.out.println("Enter name is valid");
+        } else {
+            System.out.println("Enter name is invalid");
+        }
+    }
+}
